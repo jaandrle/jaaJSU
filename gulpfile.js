9e4ac184b5d8ac5cd6c18dc3ab812a61a6adeb36
Jan Andrle <andrle.jan@centrum.cz>
2018-11-20 10:57:52 +0100

    :hammer: gulp4 and replacing settings by package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 /* jshint esversion: 6, undef: true, node: true */
 /* \CONFIG\ */
 let config= (function(){
+        let $o_default= {spawn: require('child_process').spawn, fs: require("fs")};
         const gulp= require('gulp'),
               $gulp_folder= "./gulp/",
               $run= require($gulp_folder+'gulp-crossplatform')(),
-              {$g,$o}= require($gulp_folder+'gulp-requires')(gulp),
-              app= JSON.parse($o.fs.readFileSync('./package.json'));
+              {fullName, version, build, src_folder, bin_folder, standalone, namespaces_rename, sequence, dependencies}= JSON.parse($o_default.fs.readFileSync('./package.json')),
+              {$g,$o}= mapDependencies(dependencies, $o_default);
+        const app= {name: fullName, version, build, src_folder, bin_folder, standalone, namespaces_rename, sequence};
         return {gulp, $gulp_folder, $run, $g, $o, app, error: error()};
 })();
 /* /CONFIG/ */
@@ -22,5 +24,21 @@ function error(){
     function addNum(num=1){ if_error+= num; }
     function handler(err){  addNum(); config.$g.util.log(config.$g.util.colors.red('[Error]'), err.toString()); }
     return { getText, addText, getNum, addNum, handler };
+}
+function mapDependencies(dependencies, $o_default){
+    const dependencies_keys= Object.keys(dependencies);
+    const pre= "gulp-";
+    const rename= {"gulp-minify": "gulp-minify_js", "gulp-javascript-obfuscator": "gulp-js_obfuscator"}, rename_keys= Object.keys(rename);
+    let out= {$g: {} /* for "gulp-" */ , $o: $o_default /* for others */};
+
+    dependencies_keys.forEach(cmd=>{
+        if(cmd==="gulp") return false;
+        let out_key= "$o";
+        const name= rename_keys.indexOf(cmd)!==-1 ? rename[cmd].replace(pre, setTo$g) : cmd.replace(pre, setTo$g);
+        out[out_key][name]= require(cmd);
+
+        function setTo$g(...arg){ if(arg.length){ out_key= "$g"; } return ""; }
+    });
+    return out;
 }
 /* /Global functions/ */
