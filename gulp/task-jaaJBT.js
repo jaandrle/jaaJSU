c765a4fac6956fdd6b4e9becdda8ba880bd448da
Jan Andrle <andrle.jan@centrum.cz>
2019-11-28 15:11:25 +0100

    Automate `jaaJBT.json` generation

---
+++ gulp/task-jaaJBT.js
@@ -0,0 +1,17 @@
+/* jshint esversion: 6,-W097, -W040, node: true, expr: true, undef: true */
+module.exports= function({ app, $gulp_folder, gulp, error, $g, $o, $run }){
+    const jaaJBT_start= {
+        config: {
+            root_url: "https://raw.githubusercontent.com/jaandrle/jaaJSU/master/"
+        },
+        scripts: {}
+    };
+    return function(cb){
+        jaaJBT_start.scripts[ `jaaJSU_${app.standalone}` ]= {
+            version: app.version,
+            src: app.bin_folder+"jaaJSU.js",
+            target_path: "app_js_src_path"
+        };
+        $o.fs.writeFile("jaaJBT.json", JSON.stringify(jaaJBT_start, null, "    "), cb);
+    };
+};
