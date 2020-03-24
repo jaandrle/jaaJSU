a74245938cbc974742611a3899f075dbcd1ba80b
Jan Andrle <andrle.jan@centrum.cz>
2020-03-24 15:11:23 +0100

    Update gulp/task-jaaJBT.js

--- gulp/task-jaaJBT.js
+++ gulp/task-jaaJBT.js
@@ -3,7 +3,7 @@ module.exports= function({ app, $gulp_folder, gulp, error, $g, $o, $run }){
     const jaaJBT_start= {
         config: {
             version: "1.1.0",
-            root_url: "https://raw.githubusercontent.com/jaandrle/jaaJSU/v0.9/"
+            root_url: "https://raw.githubusercontent.com/jaandrle/jaaJSU/master/"
         },
         scripts: {}
     };
